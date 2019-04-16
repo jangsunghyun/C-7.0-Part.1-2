285dedb369778cc003d628dda0748cb69bb9b642
jangsunghyun <mokacoffee1@nate.com>
2019-04-16 09:30:14 +0900

    StaticEx

---
+++ StaticEx2/StaticEx2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticEx2
+{
+    class MyClass
+    {
+        public static int Global_count = 0;
+    }
+
+    class DemoClass1
+    {
+        public class DemoClass1
+        {
+            MyClass.Global_count++;
+        }
+    }
+
+    class DemoClass2 {
+        public class DemoClass2
+        {
+
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
